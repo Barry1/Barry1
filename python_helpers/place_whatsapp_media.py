d4e858f2b90e17fec364c71328525e3af0dfdbcc
Ebeling, Dr. Bastian <bastian.ebeling@web.de>
2021-08-01 20:31:27 +0200

    Small helper moving WA-Media-Files in Datewise-Subfolders

---
+++ python_helpers/place_whatsapp_media.py
@@ -0,0 +1,29 @@
+"""traverses through moved WhatsApp media folder and moves files into date-folders"""
+import glob
+import os
+
+BASEDIRTOWORKIN = "/mnt/rsyncdir/"
+
+
+def placefile(completefilepath: str) -> None:
+    """Finds Date from Filename, creates Folder and moves into."""
+    basepath = os.path.dirname(completefilepath)
+    targetpath = basepath + os.sep + os.path.basename(completefilepath).split("-")[1]
+    print(f"We should move {completefilepath} into {targetpath}")
+    if not os.path.isdir(targetpath):
+        os.mkdir(targetpath)
+    os.rename(
+        completefilepath, targetpath + os.sep + os.path.basename(completefilepath)
+    )
+
+
+if __name__ == "__main__":
+    for wrkpath in [
+        BASEDIRTOWORKIN + a
+        for a in os.listdir(BASEDIRTOWORKIN)
+        if a.find("WhatsApp") == 0 and a.find("WhatsApp Documents") == -1
+    ]:
+        for foundfile in glob.glob(wrkpath + os.sep + "*"):
+            if os.path.isfile(foundfile):
+                print(foundfile)
+                placefile(foundfile)
