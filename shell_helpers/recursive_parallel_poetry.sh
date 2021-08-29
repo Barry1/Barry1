a89ad07279332a2900123d45a1f4c1c781cd17d5
Ebeling, Dr. Bastian <bastian.ebeling@web.de>
2021-08-29 14:52:51 +0200

    some improvements

--- shell_helpers/recursive_parallel_poetry.sh
+++ shell_helpers/recursive_parallel_poetry.sh
@@ -4,26 +4,39 @@
 # exec 1>&2
 # https://www.cyberciti.biz/tips/bash-shell-parameter-substitution-2.html
 cdpoetryupdate () {
+    echo $0
+    echo $1
     echo ${1%/poetry.lock}
     pushd ${1%/poetry.lock}
     if [ $(which niceload) ]
     then
-        niceload poetry update
+        niceload poetry --quiet update
     else
-        poetry update
+        poetry --quiet update
     fi
     git stage poetry.lock
     git commit -m "poetry update" poetry.lock
     git push
     popd
-
 }
 # make function available
 export -f cdpoetryupdate #for bash
 # do the work
 # https://bash.cyberciti.biz/guide/Default_shell_variables_value
 # default = current folder - else $1
-workdir=${1:-.}
+case "$1" in
+*/)
+    tempworkdir=$1
+    ;;
+*)
+    tempworkdir="$1/"
+    ;;
+esac
+workdir=${tempworkdir:-.}
+echo "Working in $workdir"
 poetrylockfilelocations=`find $workdir -name poetry\.lock 2>/dev/null`
+parallel --verbose echo ::: $poetrylockfilelocations
+echo "==============================================================="
 # https://www.gnu.org/software/parallel/
-parallel cdpoetryupdate ::: $poetrylockfilelocations
+parallel --verbose cdpoetryupdate ::: $poetrylockfilelocations
+# find $workdir -name poetry\.lock -print0 2>/dev/null | xargs --verbose --max-procs=0 --null -I {} bash -c 'cdpoetryupdate {}'
