eecefda07a8540bd0d9cfd1834f8d5219367a562
Ebeling, Dr. Bastian <bastian.ebeling@web.de>
2021-08-30 13:33:24 +0200

    some better bash programming, next step is improvement for dir by parallel

--- shell_helpers/recursive_parallel_poetry.sh
+++ shell_helpers/recursive_parallel_poetry.sh
@@ -4,9 +4,6 @@
 # exec 1>&2
 # https://www.cyberciti.biz/tips/bash-shell-parameter-substitution-2.html
 cdpoetryupdate () {
-    echo $0
-    echo $1
-    echo ${1%/poetry.lock}
     pushd ${1%/poetry.lock}
     if [ $(which niceload) ]
     then
@@ -24,19 +21,11 @@ export -f cdpoetryupdate #for bash
 # do the work
 # https://bash.cyberciti.biz/guide/Default_shell_variables_value
 # default = current folder - else $1
-case "$1" in
-*/)
-    tempworkdir=$1
-    ;;
-*)
-    tempworkdir="$1/"
-    ;;
-esac
-workdir=${tempworkdir:-.}
+workdir=${1:-.}
 echo "Working in $workdir"
-poetrylockfilelocations=`find $workdir -name poetry\.lock 2>/dev/null`
-parallel --verbose echo ::: $poetrylockfilelocations
+poetrylockfilelocations=`find $workdir"/" -name poetry\.lock 2>/dev/null`
+parallel --group  echo "found" ::: $poetrylockfilelocations
 echo "==============================================================="
 # https://www.gnu.org/software/parallel/
-parallel --verbose cdpoetryupdate ::: $poetrylockfilelocations
+parallel --group --verbose cdpoetryupdate ::: $poetrylockfilelocations
 # find $workdir -name poetry\.lock -print0 2>/dev/null | xargs --verbose --max-procs=0 --null -I {} bash -c 'cdpoetryupdate {}'
