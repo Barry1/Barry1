7de8aa1cca8a69b36f5a0dc7f289ef2cbe312470
Ebeling, Dr. Bastian <230051+Barry1@users.noreply.github.com>
2021-10-31 00:26:03 +0200

    env/bash shebang

--- shell_helpers/instpoetry.sh
+++ shell_helpers/instpoetry.sh
@@ -1,3 +1,5 @@
+#!/bin/env bash
+#https://python-poetry.org/docs/master/#installation
 pyexe=$(which python3)
 poetryinstallsrc=https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py
 poetrygitsrc=https://github.com/python-poetry/poetry.git@master
