11e04c8b0df739d5fb305f696f4236a72d06f7fd
Ebeling, Dr. Bastian <230051+Barry1@users.noreply.github.com>
2021-10-31 00:23:08 +0200

    abstraction

--- shell_helpers/instpoetry.sh
+++ shell_helpers/instpoetry.sh
@@ -1,8 +1,10 @@
 pyexe=$(which python3)
+poetryinstallsrc=https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py
+poetrygitsrc=https://github.com/python-poetry/poetry.git@master
 
 $pyexe -m ensurepip
 $pyexe -m pip install --upgrade pip
 
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | $pyexe - --git https://github.com/python-poetry/poetry.git@master
+curl -sSL $poetryinstallsrc | $pyexe - --git $poetrygitsrc
 
 poetry self update
