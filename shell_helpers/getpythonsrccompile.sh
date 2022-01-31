7b55ca0aa0e0d8163dd69291d6bc390a6f11cd2f
Ebeling, Dr. Bastian <230051+Barry1@users.noreply.github.com>
2022-01-31 13:14:10 +0100

    beautysh

--- shell_helpers/getpythonsrccompile.sh
+++ shell_helpers/getpythonsrccompile.sh
@@ -1,21 +1,21 @@
 echo preparing environment
 sudo apt-get install \
-build-essential \
-gdb \
-lcov \
-libbz2-dev \
-libffi-dev \
-libgdbm-dev \
-liblzma-dev \
-libncurses5-dev \
-libreadline6-dev \
-libsqlite3-dev \
-libssl-dev \
-lzma \
-lzma-dev \
-tk-dev \
-uuid-dev \
-zlib1g-dev
+    build-essential \
+    gdb \
+    lcov \
+    libbz2-dev \
+    libffi-dev \
+    libgdbm-dev \
+    liblzma-dev \
+    libncurses5-dev \
+    libreadline6-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    lzma \
+    lzma-dev \
+    tk-dev \
+    uuid-dev \
+    zlib1g-dev
 echo Downloading
 wget https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz
 echo Extracting
