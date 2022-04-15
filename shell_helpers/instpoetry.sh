f9d2554862a4c77e6e8fb392009cec2e628c69f5
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2022-04-15 21:48:33 +0200

    neue Quelle für Installationsskript

--- shell_helpers/instpoetry.sh
+++ shell_helpers/instpoetry.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 #https://python-poetry.org/docs/master/#installation
+sudo apt install python3-venv
 pyexe=$(which python3)
-poetryinstallsrc=https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py
+#poetryinstallsrc=https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py
+poetryinstallsrc=https://install.python-poetry.org/
 poetrygitsrc=https://github.com/python-poetry/poetry.git@master
 
-$pyexe -m ensurepip
-$pyexe -m pip install --upgrade pip
+#not needed
+#$pyexe -m ensurepip
+#$pyexe -m pip install --upgrade pip
 
 curl -sSL $poetryinstallsrc | $pyexe - --git $poetrygitsrc --force --yes
+
+export PATH="/home/ebeling/.local/bin:$PATH"
 
 poetry self update
