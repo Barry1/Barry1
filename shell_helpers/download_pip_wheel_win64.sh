2dad7e5cb2634a914b13c5f1ff1c50c0d1c5b9be
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2022-04-28 17:00:19 +0200

    schon recht gut

--- shell_helpers/download_pip_wheel_win64.sh
+++ shell_helpers/download_pip_wheel_win64.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/sh
-python3 -m pip download --python-version 3.10 --platform win_amd64 --platform none --only-binary=:all: $@
-echo "If errors - try \"python3 -m pip download PCKNAME\""
+download_whl_or_src () {
+    echo $1
+    python3 -m pip download --quiet --python-version 3.10 --platform win_amd64 --platform none --only-binary=:all: $1
+    if [ $? -eq 1 ]
+    then
+        python3 -m pip download --quiet --no-binary=:all: $1
+    fi
+}
+for x in $@; do
+    download_whl_or_src $x
+done
