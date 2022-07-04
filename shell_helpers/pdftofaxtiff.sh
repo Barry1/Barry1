0ca665ee248acfb4fa050b03ee944b71aff79fe2
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2022-07-04 11:47:33 +0200

    convert (multipage) PDF to G4 standard (mulitpage) TIFF

---
+++ shell_helpers/pdftofaxtiff.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+# This script could reside in ~/.local/bin or better be linked
+# ln /home/ebeling/GitHub/Barry1/shell_helpers/pdftofaxtiff.sh ~/.local/bin/pdftofaxtiff.sh
+# and the folder should be in your path
+
+# inspired by <https://stackoverflow.com/a/10045299/617339>
+
+gs \
+    -o "${1%.pdf}_fax.tiff"\
+    -sDEVICE=tiffg4\
+    "$1"
