acc19d550bec400ded6071db196fe54b0c49ffc8
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2022-07-21 14:00:04 +0200

    Formatting

--- python_helpers/place_whatsapp_media.py
+++ python_helpers/place_whatsapp_media.py
@@ -2,13 +2,16 @@
 import glob
 import os
 from typing import Literal
-BASEDIRTOWORKIN:Literal['/mnt/rsyncdir/'] = "/mnt/rsyncdir/"
+
+BASEDIRTOWORKIN: Literal["/mnt/rsyncdir/"] = "/mnt/rsyncdir/"
 
 
 def placefile(completefilepath: str) -> None:
     """Find Date from Filename, create Folder and move into."""
-    basepath:str = os.path.dirname(completefilepath)
-    targetpath:str = basepath + os.sep + os.path.basename(completefilepath).split("-")[1]
+    basepath: str = os.path.dirname(completefilepath)
+    targetpath: str = (
+        basepath + os.sep + os.path.basename(completefilepath).split("-")[1]
+    )
     print(f"We should move {completefilepath} into {targetpath}")
     if not os.path.isdir(targetpath):
         os.makedirs(targetpath)
