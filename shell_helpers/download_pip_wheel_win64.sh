8ff78819d12bfae7bef2f426b7472df8ef400896
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2022-09-14 08:00:49 +0200

    shellcheck

--- shell_helpers/download_pip_wheel_win64.sh
+++ shell_helpers/download_pip_wheel_win64.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 download_whl_or_src () {
-    echo $1
-    python3 -m pip download --quiet --python-version 3.10 --platform win_amd64 --platform none --only-binary=:all: $1 2>/dev/null
+    echo "$1"
+    python3 -m pip download --quiet --python-version 3.10 --platform win_amd64 --platform none --only-binary=:all: "$1" 2>/dev/null
     if [ $? -eq 1 ]
     then
-        python3 -m pip download --quiet --no-binary=:all: $1
+        python3 -m pip download --quiet --no-binary=:all: "$1"
     fi
 }
 export -f download_whl_or_src #for bash
@@ -12,4 +12,4 @@ export -f download_whl_or_src #for bash
 #    download_whl_or_src $x
 #done
 #https://www.gnu.org/software/parallel/parallel_alternatives.html#DIFFERENCES-BETWEEN-xargs-AND-GNU-Parallel
-parallel download_whl_or_src {} --nice 18 ::: $@
+parallel download_whl_or_src {} --nice 18 ::: "$@"
