000146ac25b5aa558d766fa43c602f8cfc393df1
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2022-09-14 12:39:56 +0200

    shellcheck

--- shell_helpers/merge_gist_into_git.sh
+++ shell_helpers/merge_gist_into_git.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 [ ! $# -eq 2 ] && { echo "Usage: $0 GIST_URL GIT_FOLDER"; exit 1; }
 echo "Merging GIST $1 into GIT folder $2"
-pushd $2 >/dev/null
-git remote add --fetch gisttoimport $1
+pushd "$2" >/dev/null || exit
+git remote add --fetch gisttoimport "$1"
 git merge -m "gist $1 merged into" gisttoimport --allow-unrelated-histories
 git push
 git remote remove gisttoimport
-popd >/dev/null
+popd >/dev/null || exit
