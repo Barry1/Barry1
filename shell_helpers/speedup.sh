f0dffe9b8a0ffd8c89abfc901aff1121d0cf3403
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2022-10-08 13:36:57 +0200

    decrement reduced

--- shell_helpers/speedup.sh
+++ shell_helpers/speedup.sh
@@ -3,5 +3,5 @@
 # for further explanation see
 # <https://stackoverflow.com/a/28099707/207661>
 # or <http://wiki.bash-hackers.org/scripting/posparams>
-NICEADJUSTMENT=-17
+NICEADJUSTMENT=-7
 sudo nice --adjustment=$NICEADJUSTMENT runuser -u "$USER" "$@"
