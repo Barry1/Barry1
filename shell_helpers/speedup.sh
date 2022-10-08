72b8c0b413946e563c54554914924f19fe8e6ed4
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2022-10-08 13:03:56 +0200

    first implementation

---
+++ shell_helpers/speedup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# $* and $@ deliver all arguments
+# for further explanation see
+# <https://stackoverflow.com/a/28099707/207661>
+# or <http://wiki.bash-hackers.org/scripting/posparams>
+NICEADJUSTMENT=-17
+sudo nice --adjustment=$NICEADJUSTMENT runuser -u "$USER" "$*"
