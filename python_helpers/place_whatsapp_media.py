7c8678ec4dbabf3177ffe84eb4829a2c8b6a1ec5
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2022-12-29 11:46:18 +0100

    poetry update

--- python_helpers/place_whatsapp_media.py
+++ python_helpers/place_whatsapp_media.py
@@ -3,31 +3,32 @@ import glob
 import os
 from typing import Literal
 
-BASEDIRTOWORKIN: Literal["/mnt/rsyncdir/"] = "/mnt/rsyncdir/"
+BASEDIRTOWORKIN: Literal['/mnt/rsyncdir/'] = '/mnt/rsyncdir/'
 
 
 def placefile(completefilepath: str) -> None:
     """Find Date from Filename, create Folder and move into."""
     basepath: str = os.path.dirname(completefilepath)
     targetpath: str = (
-        basepath + os.sep + os.path.basename(completefilepath).split("-")[1]
+        basepath + os.sep + os.path.basename(completefilepath).split('-')[1]
     )
-    print(f"We should move {completefilepath} into {targetpath}")
+    print(f'We should move {completefilepath} into {targetpath}')
     if not os.path.isdir(targetpath):
         os.makedirs(targetpath)
     os.rename(
-        completefilepath, targetpath + os.sep + os.path.basename(completefilepath)
+        completefilepath,
+        targetpath + os.sep + os.path.basename(completefilepath),
     )
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     for wrkpath in [
         BASEDIRTOWORKIN + a
         for a in os.listdir(BASEDIRTOWORKIN)
-        if a.find("WhatsApp") == 0 and a.find("WhatsApp Documents") == -1
+        if a.find('WhatsApp') == 0 and a.find('WhatsApp Documents') == -1
     ]:
-        for foundfile in glob.glob(wrkpath + os.sep + "*"):
+        for foundfile in glob.glob(wrkpath + os.sep + '*'):
             if os.path.isfile(foundfile):
                 print(foundfile)
                 placefile(foundfile)
-    print("Ready.")
+    print('Ready.')
