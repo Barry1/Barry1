36fcd4875bf8305a42d2bcb52c97534731f9d8eb
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2023-01-11 12:54:09 +0100

    working as expected

--- shell_helpers/update_plantuml.sh
+++ shell_helpers/update_plantuml.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # https://linuxconfig.org/download-file-from-url-on-linux-using-command-line
-#sudo curl https://github.com/plantuml/plantuml/releases/latest/download/plantuml.jar -o /usr/share/plantuml/plantuml.jar
-sudo wget https://github.com/plantuml/plantuml/releases/latest/download/plantuml.jar --output-document=/usr/share/plantuml/plantuml.jar
+echo Downloading new version
+sudo curl --location https://github.com/plantuml/plantuml/releases/latest/download/plantuml.jar --output /usr/share/plantuml/plantuml.jar
+# sudo wget https://github.com/plantuml/plantuml/releases/latest/download/plantuml.jar --output-document=/usr/share/plantuml/plantuml.jar
+echo Hard-linking .vscode-extensions
 find /home/ebeling/.vscode/extensions/ -name plantuml.jar -exec ln --backup --suffix=orig /usr/share/plantuml/plantuml.jar \{\} \;
