0b4c17914f46e07b04db62beaf0ec4dc092d3a65
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2023-01-11 12:57:17 +0100

    progress bar for download

--- shell_helpers/update_plantuml.sh
+++ shell_helpers/update_plantuml.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # https://linuxconfig.org/download-file-from-url-on-linux-using-command-line
+# https://www.thegeekstuff.com/2012/07/wget-curl/
 echo Downloading new version
-sudo curl --location https://github.com/plantuml/plantuml/releases/latest/download/plantuml.jar --output /usr/share/plantuml/plantuml.jar
+sudo curl --progress-bar --location https://github.com/plantuml/plantuml/releases/latest/download/plantuml.jar --output /usr/share/plantuml/plantuml.jar
 # sudo wget https://github.com/plantuml/plantuml/releases/latest/download/plantuml.jar --output-document=/usr/share/plantuml/plantuml.jar
 echo Hard-linking .vscode-extensions
 find /home/ebeling/.vscode/extensions/ -name plantuml.jar -exec ln --backup --suffix=orig /usr/share/plantuml/plantuml.jar \{\} \;
