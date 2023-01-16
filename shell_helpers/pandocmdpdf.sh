3ad20c0a517f6ec322656958436d8378600e3bf2
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2023-01-16 23:33:29 +0100

    great job

--- shell_helpers/pandocmdpdf.sh
+++ shell_helpers/pandocmdpdf.sh
@@ -1,13 +1,47 @@
 #!/bin/sh
 
+USE_KROKI=1
+
+set -- --output "${1%.md}.pdf" "$@"
+
+
+PANDOC_DIR=$(pandoc -v | grep "User data directory" | awk --field-separator : '{print $2}')
+## remove leading spaces
+PANDOC_DIR=${PANDOC_DIR## }
+
+set -- "--pdf-engine=xelatex" "$@"
+set -- "--variable=author:Dr. Bastian Ebeling" "$@"
+set -- "--variable=papersize:a4" "$@"
+set -- "--variable=colorlinks" "$@"
+set -- "--variable=documentclass:scrartcl" "$@"
+set -- "--table-of-contents" "$@"
+set -- "--from=markdown+smart+auto_identifiers+fancy_lists+task_lists+definition_lists+definition_lists+table_captions+pipe_tables+yaml_metadata_block+footnotes+citations+emoji+abbreviations+autolink_bare_uris" "$@"
+
+if [ "$USE_KROKI" = 1 ]
+then
+    set -- "--filter=pandoc-kroki" "$@"
+else
+    set -- "--metadata=plantumlPath:/usr/share/plantuml/plantuml.jar" "$@"
+    set -- "--lua-filter=${PANDOC_DIR}/filters/diagram-generator.lua" "$@"
+    set -- "--filter=mermaid-filter" "$@"
+    # not needed as better in lua-filter
+    # set -- "--filter=pandoc-plantuml" "$@"
+fi
+
 prepare() {
-	sudo apt-get install --upgrade pandoc-plantuml-filter
-	sudo npm install --global mermaid-filter
+    sudo apt-get install --upgrade pandoc-plantuml-filter librsvg2-bin
+    sudo npm install --global mermaid-filter
+    pip install "git+https://gitlab.com/myriacore/pandoc-kroki-filter.git"
+    echo "CHANGE NEEDED"
+    echo "Add \"vega-lite\": \"vegalite\" to DIAGRAM_SYNONYMNS"
+    #https://github.com/pandoc/lua-filters#installation
+    #pandoc -v
+    PANDOC_DIR=/home/ebeling/.local/share/pandoc
+    RELEASE_URL=https://github.com/pandoc/lua-filters/releases/latest
+    curl -LSs $RELEASE_URL/download/lua-filters.tar.gz | \
+    tar --strip-components=1 --one-top-level=$PANDOC_DIR -zvxf -
 }
 
-pandoc --pdf-engine=xelatex \
-	--variable=author:"Dr. Bastian Ebeling" --variable=papersize:a4 --variable=colorlinks \
-	--variable=documentclass:scrartcl --table-of-contents --listings \
-	--filter pandoc-plantuml \
-	--filter mermaid-filter \
-	--output="${1%.md}.pdf" "$1"
+pandoc "$@"
+
+
