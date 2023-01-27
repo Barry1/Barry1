cc077a6e80de5351eb57ad3f14c53172be42792a
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2023-01-27 23:48:16 +0100

    execdir instead of exec

--- shell_helpers/trunkupd.sh
+++ shell_helpers/trunkupd.sh
@@ -3,5 +3,6 @@ BASEDIR=${1:-/home/ebeling/GitHub}
 echo "Updating all trunk below $BASEDIR"
 find $BASEDIR -name .trunk -execdir trunk upgrade \;
 find $BASEDIR -name trunk.yaml -exec sed 's/black@22.12.0/black@23.1a1/' --in-place \{\} + -execdir trunk install \;
-find $BASEDIR -name trunk.yaml -exec grep black \{\} \;
-find $BASEDIR -name .trunk -exec trunk check --all \;
+#find $BASEDIR -name trunk.yaml -exec grep black \{\} \;
+find $BASEDIR -name .trunk -execdir trunk check --all \;
+
