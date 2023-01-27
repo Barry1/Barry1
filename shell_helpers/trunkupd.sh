e66fac32b256cedec7faa8bfad847c176d46cb03
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2023-01-27 23:52:41 +0100

    enable git-hooks

--- shell_helpers/trunkupd.sh
+++ shell_helpers/trunkupd.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 BASEDIR=${1:-/home/ebeling/GitHub}
 echo "Updating all trunk below $BASEDIR"
-find $BASEDIR -name .trunk -execdir trunk upgrade \;
-find $BASEDIR -name trunk.yaml -exec sed 's/black@22.12.0/black@23.1a1/' --in-place \{\} + -execdir trunk install \;
-#find $BASEDIR -name trunk.yaml -exec grep black \{\} \;
-find $BASEDIR -name .trunk -execdir trunk check --all \;
-find $BASEDIR -name .trunk -execdir trunk actions enable trunk-fmt-pre-commit trunk-check-pre-push \;
+find "$BASEDIR" -name .trunk -execdir trunk upgrade \;
+find "$BASEDIR" -name trunk.yaml -exec sed 's/black@22.12.0/black@23.1a1/' --in-place \{\} + -execdir trunk install \;
+#find "$BASEDIR" -name trunk.yaml -exec grep black \{\} \;
+find "$BASEDIR" -name .trunk -execdir trunk check --all \;
+find "$BASEDIR" -name .trunk -execdir trunk actions enable trunk-fmt-pre-commit trunk-check-pre-push \;
