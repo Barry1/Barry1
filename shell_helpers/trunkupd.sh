4b5588f3c2581b9eaa070421b428a029885d8e68
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2023-02-18 20:54:38 +0100

    solltr passen

--- shell_helpers/trunkupd.sh
+++ shell_helpers/trunkupd.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 BASEDIR=${1:-/home/ebeling/GitHub}
 echo "Updating all trunk below $BASEDIR"
-find "$BASEDIR" -name .trunk -execdir trunk upgrade \;
+find "$BASEDIR" -name .trunk \
+	-not -path "*venv*" \
+	-print \
+	-execdir trunk upgrade \;
+echo "Adding numpy to all trunk-pythons (numpy.typing->mypy)"
+find ~/.cache/trunk/tools/python -name python3 \
+	-print \
+	-execdir ./python3 -m pip install --upgrade pip numpy \;
 #No longer exception for black needed
 #find "$BASEDIR" -name trunk.yaml -exec sed 's/black@22.12.0/black@23.1a1/' --in-place \{\} + -execdir trunk install \;
 #find "$BASEDIR" -name trunk.yaml -exec grep black \{\} \;
 find "$BASEDIR" -name .trunk \
+	-not -path "*venv*" \
+	-print \
 	-execdir trunk check --all \; \
 	-execdir trunk actions enable trunk-fmt-pre-commit trunk-check-pre-push \;
