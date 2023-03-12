a8681b1136fc481eccca90efe59651303ada0795
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2023-03-12 15:48:43 +0100

    typo

--- shell_helpers/picrename.sh
+++ shell_helpers/picrename.sh
@@ -2,10 +2,10 @@
 
 # Heimliche HEVC-Dateien von .jpeg in .heic umbenennen
 #find . -name "*.jpeg" -execdir sh -c 'file "{}" | grep HEVC && mv "{}" "{}.heic" ' \;
-find . -name "*.jpeg" -print -execdir sh -c 'file "$1" | grep HEVC && mv "$1" "${%1.jpeg}".heic' -- '{}' \;
+find . -name "*.jpeg" -print -execdir sh -c 'file "$1" | grep HEVC && mv "$1" "${1%.jpeg}".heic' -- \{\} \;
 
 # .JPG in .jpg umbenennen
 #find . -name "*.JPG" -execdir sh -c 'echo "$1" "${1%.JPG}".jpg' - '{}' +
-find . -name "*.JPG" -print -execdir sh -c 'mv "$1" "${1%.JPG}".jpg' -- '{}' \;
+find . -name "*.JPG" -print -execdir sh -c -v 'echo "$1" "${1%.JPG}".jpg' -- \{\} \;
 
 #find . -name "*.t1" -exec bash -c 'mv "$1" "${1%.t1}".t2' - '{}' +
