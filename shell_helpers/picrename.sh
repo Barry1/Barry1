3f89273a77578e483273a2a7c2db0588bf3644e1
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2023-03-12 15:29:02 +0100

    even better

--- shell_helpers/picrename.sh
+++ shell_helpers/picrename.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+
 # Heimliche HEVC-Dateien von .jpeg in .heic umbenennen
-find . -name *.jpeg -execdir sh -c 'file "{}" | grep HEVC && mv "{}" "{}.heic" ' \;
+#find . -name "*.jpeg" -execdir sh -c 'file "{}" | grep HEVC && mv "{}" "{}.heic" ' \;
+find . -name "*.jpeg" -print -execdir sh -c 'file "$1" | grep HEVC && mv "$1" "${%1.jpeg}".heic' -- '{}' \;
+
 # .JPG in .jpg umbenennen
-find . -name *.JPG -execdir mv \{\} \{\}.jpg \;
-#ISO Media, HEIF Image HEVC Main or Main Still Picture Profile
+#find . -name "*.JPG" -execdir sh -c 'echo "$1" "${1%.JPG}".jpg' - '{}' +
+find . -name "*.JPG" -print -execdir sh -c 'mv "$1" "${1%.JPG}".jpg' -- '{}' \;
+
+#find . -name "*.t1" -exec bash -c 'mv "$1" "${1%.t1}".t2' - '{}' +
