400db4ba7defaea4c50301a1651fa30b5a7de9f5
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2023-03-17 09:38:26 +0100

    3,5 GiB and saved device Name

--- shell_helpers/zRAM.sh
+++ shell_helpers/zRAM.sh
@@ -3,6 +3,6 @@
 sudo modprobe zram
 # sudo zramctl --find --size 3500MiB
 zDEVICE=$(sudo zramctl --find --size 3,5GiB)
-sudo mkswap "$zDEVICE"
-sudo swapon "$zDEVICE" --priority 4478
+sudo mkswap "${zDEVICE}"
+sudo swapon "${zDEVICE}" --priority 4478
 sudo zramctl--output-all
