b1a1b152512478412312390aec2ccea894126f3c
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2023-03-17 09:37:21 +0100

    3,5 GiB and saved device Name

--- shell_helpers/zRAM.sh
+++ shell_helpers/zRAM.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+# https://www.linux-community.de/ausgaben/linuxuser/2018/07/sauber-entlueftet/2/
 sudo modprobe zram
-sudo zramctl --find --size 2GiB
-sudo mkswap /dev/zram0
-sudo swapon /dev/zram0 --priority 4478
-#https://www.linux-community.de/ausgaben/linuxuser/2018/07/sauber-entlueftet/2/
+# sudo zramctl --find --size 3500MiB
+zDEVICE=$(sudo zramctl --find --size 3,5GiB)
+sudo mkswap "$zDEVICE"
+sudo swapon "$zDEVICE" --priority 4478
+sudo zramctl--output-all
