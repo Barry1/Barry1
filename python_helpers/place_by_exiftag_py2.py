698bc9593569dc5098c7f8dac6c93ee36803f5b2
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2023-05-01 21:01:28 +0200

    finally working on by QNAP

---
+++ python_helpers/place_by_exiftag_py2.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2.7
+import glob
+import os
+
+import piexif
+
+# https://pypi.org/project/piexif
+# curl  -O -J https://files.pythonhosted.org/packages/fa/84/a3f25cec7d0922bf60be8000c9739d28d24b6896717f44cc4cfb843b1487/piexif-1.1.3.zip
+# unzip piexif-1.1.3.zip
+# mv piexif /mnt/ext/opt/Python/lib/python2.7/site-packages/
+
+# formatted with `autopep8 --in-place place_by_exiftag_py2.py`
+# formatted with `isort --in-place place_by_exiftag_py2.py`
+
+
+def processfile(thepath, thefilename):
+    srcpath = thepath + os.sep + thefilename
+    exif_dict = piexif.load(srcpath)
+    thetagdata = exif_dict["Exif"][36867]  # DateTimeOriginal
+    dasjahr = thetagdata[:4]
+    dermonat = thetagdata[5:7]
+    dertag = thetagdata[8:10]
+    targetfolder = thepath + os.sep + dasjahr + os.sep + dasjahr + dermonat + dertag
+    try:
+        os.makedirs(targetfolder)
+    except OSError as theerr:
+        # print(theerr)
+        pass
+    # Dateien (egal welche Erweiterung) verschieben
+    for gleichedatei in glob.glob(srcpath[:-4] + "*"):
+        try:
+            os.rename(gleichedatei, targetfolder + os.sep + gleichedatei[3:])
+        except OSError as theerr:
+            print(theerr)
+
+
+if __name__ == "__main__":
+    thepath = "/share/BastiPhone"
+    alledateien = os.listdir(thepath)
+    for gefunden in alledateien:
+        if gefunden.endswith(".jpg") and gefunden.startswith("IMG_"):
+            processfile(thepath, gefunden)
