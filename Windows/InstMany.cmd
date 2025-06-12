4555054e732379396625fbc1f945fea92e6657f6
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2025-06-12 09:56:37 +0200

    some Windows winget commands

---
+++ Windows/InstMany.cmd
@@ -0,0 +1,21 @@
+ECHO "We install many useful programs"
+REM winget list --source msstore
+winget install --source msstore "draw.io Diagrams"
+winget install --id MarekJasinski.FreeCommanderXE
+winget install FreeMind
+winget install --id Ghostgum.GSview
+winget install --id Git.Git
+winget install --id Ghisler.TotalCommander
+winget install --id GNU.Octave
+winget install --id Greenshot.Greenshot
+winget install --id JohnMacFarlane.Pandoc
+winget install --id LyX.LyX
+winget install --id MiKTeX.MiKTeX
+winget install --id Notepad++.Notepad++
+winget install --id dotPDN.PaintDotNet
+winget install --source msstore "Python 3.13"
+winget install --id Posit.Quarto
+winget install --id SQLite.SQLite
+winget install --exact --name "Visual Studio Code"
+winget install --source msstore "Vivaldi"
+winget install WinPlantUml
