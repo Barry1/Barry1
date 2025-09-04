65cd1580acec08689724ca51d92abf82cbcc004e
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2025-09-04 21:54:23 +0200

    no trunk blames

--- shell_helpers/pyenvup.sh
+++ shell_helpers/pyenvup.sh
@@ -2,7 +2,8 @@
 pyenv update
 #for a in {7..13}; do echo pyenv install --skip-existing $(pyenv latest --known 3.$a) ; done
 a=7
-while [ $a -le 13 ]; do
-	pyenv install --skip-existing "$(pyenv latest --known 3.$a)"
+while [ "${a}" -le 13 ]; do
+	# shellcheck disable=SC2312
+	pyenv install --skip-existing "$(pyenv latest --known 3."${a}")"
 	a=$((a + 1))
 done
