d39aa9c2c0216fb35f51bb22bee35c1e7aa9b337
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2025-09-14 23:05:23 +0200

    new opts

--- shell_helpers/buildrollingstablekernel.sh
+++ shell_helpers/buildrollingstablekernel.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # formatted with shfmt --posix --simplify --write THISFILE.sh
-
+# https://docs.kernel.org/admin-guide/quickly-build-trimmed-linux.html
+# https://medium.com/free-code-camp/building-and-installing-the-latest-linux-kernel-from-source-6d8df5345980
 # https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#Here-Documents
 
 printhelp() {
@@ -19,17 +20,20 @@ gitinit() {
 }
 buildkernel() {
 	echo ===============================================
-	rm -rf .config
 	# LMC_KEEP="fs/cifs,net/iwlwifi"
-	make "$1"
+	make "$1" rust.config x86_64_defconfig
+	# make headers
+	# make headers_install
 	echo "-basti_$1" >localversion
 	cat localversion
-	# time nice -n 20 make --jobs=$(( 2 * $(getconf _NPROCESSORS_ONLN) )) --load-average=$(getconf _NPROCESSORS_ONLN) all
+	# time nice -n 20 make --jobs=$(( 2 * $(getconf _NPROCESSORS_ONLN) )) --load-average=$(getconf _NPROCESSORS_ONLN) V=1
 	# sudo sh -c command -v installkernel && sudo make modules_install install
 	echo ===============================================
 }
 systempkginit() {
 	sudo apt install git bc bison flex libssl-dev dwarves libncurses-dev pahole
+	# for rust
+	sudo apt install bindgen rust-src
 }
 systemprep() {
 	ls /autofsmnt/Fotos >/dev/null &
