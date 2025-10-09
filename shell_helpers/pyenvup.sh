8067e4336f7c2788f5dbd612abea1a95a2531e80
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2025-10-09 20:53:48 +0200

    Python 3.14 is out

--- shell_helpers/pyenvup.sh
+++ shell_helpers/pyenvup.sh
@@ -2,7 +2,7 @@
 pyenv update
 #for a in {7..13}; do echo pyenv install --skip-existing $(pyenv latest --known 3.$a) ; done
 a=5
-while [ "${a}" -le 13 ]; do
+while [ "${a}" -le 14 ]; do
 	# shellcheck disable=SC2312
 	pyenv install --skip-existing "$(pyenv latest --known 3."${a}")"
 	a=$((a + 1))
