4501d48725d4a7c77fa68a6fb2cc81e4a53a0f72
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2026-02-06 08:44:26 +0100

    upgrade-command added

--- LinuxSetup/pytest_inst.sh
+++ LinuxSetup/pytest_inst.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 pipx install pytest
 pipx inject pytest pytest-cov pytest-sugar pytest-bandit pytest-black pytest-colordots pytest-monkeytype pytest-mypy pytest-pyright pytest-beartype pytest-asyncio
+pipx upgrade-all --include-injected
