951d0b89bc98b4c8334f7eabb79bd4ee402cebc8
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2026-02-18 14:18:33 +0100

    some changes driven by experience on new build of fastfetch

--- LinuxSetup/backportme.sh
+++ LinuxSetup/backportme.sh
@@ -11,7 +11,7 @@ echo "Installing build dependencies for $1..."
 sudo apt-get build-dep --only-source "$1"
 echo "=============================================="
 echo "Building source package for $1..."
-apt-get --build source "$1"
+apt-get --build --only-source source "$1"
 for pkg in "$1"_*.deb; do
 	echo "Installing package: ${pkg}"
 	sudo dpkg -i "${pkg}"
@@ -23,16 +23,26 @@ echo "=============================================="
 # Usage: ./backportme.sh <package-name>
 # Example: ./backportme.sh curl
 # Note: Ensure you have the necessary permissions and that rmadison is installed.
+# For Debian just run "sudo apt install devscripts" to get rmadison command.
+#### apt modernize-sources
 # Note: This script assumes you are running it in a Debian-based system with sudo privileges.
 # Note: You may need to run this script in a directory where you have write permissions.
 # Note: You may need to install 'devscripts' package to use 'rmadison' command.
 # ./backportme.sh libyyjson0
+# ./backportme.sh yyjson
+# sudo dpkg -i libyyjson0_0*.deb libyyjson-dev*.deb
 # ./backportme.sh fastfetch
 # mk-build-deps fastfetch
 # mk-build-deps --install --root-cmd sudo --remove
 # sudo dpkg -i fastfetch-build-deps_*.deb
 # sudo apt-get purge --auto-remove [package_name]-build-deps
 # https://serverfault.com/questions/127625
+
+#####
+# sudo apt-get build-dep --only-source yyjson
+# apt-get --build --only-source source yyjson
+# sudo apt-get build-dep --only-source fastfetch
+# apt-get --build --only-source source fastfetch
 
 onedriveinst() {
 	repo="https://salsa.debian.org/debian/onedrive.git"
--- LinuxSetup/my.bashrc
+++ LinuxSetup/my.bashrc
@@ -28,4 +28,38 @@ inxi
 # https://github.com/fastfetch-cli/fastfetch/wiki/Support+Status#available-modules
 # fastfetch -c all.jsonc
 # fastfetch --stat
+# fastfetch --weather-location Klein+Nordende --weather-timeout 100 --gen-config-force
 fastfetch
+# wttr.in
+# https://wttr.in/:help
+# curl http://de.wttr.in/Klein+Nordende
+# https://wttr.in/:bash.function - this is a bash function that can be sourced in .bashrc to get the weather in the terminal
+# WTTR_PARAMS is space-separated URL parameters, many of which are single characters that can be
+# lumped together. For example, "F q m" behaves the same as "Fqm".
+if [[ -z "$WTTR_PARAMS" ]]; then
+  # Form localized URL parameters for curl
+  if [[ -t 1 ]] && [[ "$(tput cols)" -lt 125 ]]; then
+      WTTR_PARAMS+='n'
+  fi 2> /dev/null
+  for _token in $( locale LC_MEASUREMENT ); do
+    case $_token in
+      1) WTTR_PARAMS+='m' ;;
+      2) WTTR_PARAMS+='u' ;;
+    esac
+  done 2> /dev/null
+  unset _token
+  export WTTR_PARAMS
+fi
+
+wttr() {
+  local location="${1// /+}"
+  test "$#" -gt 0 && shift
+  local args=()
+  for p in $WTTR_PARAMS "$@"; do
+    args+=("--data-urlencode" "$p")
+  done
+  curl --fail --get --show-error --silent -H "Accept-Language: ${LANG%_*}" "${args[@]}" --compressed "de.wttr.in/${location}"
+}
+
+wttr "Klein Nordende"
+
