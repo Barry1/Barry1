01c8c3f18cbcaff28d9e515bca15b98c14aface7
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2026-02-20 16:07:03 +0100

    works in tempdir with binpath for pyenv

--- shell_helpers/pyenvup.sh
+++ shell_helpers/pyenvup.sh
@@ -1,9 +1,15 @@
-#!/bin/sh
-pyenv update
+#!/bin/bash
+# bash needed for pushd, popd
+PYENVBIN=/home/ebeling/.pyenv/bin/pyenv
+thewrkdir=$(mktemp --directory)
+pushd "$thewrkdir" >/dev/null || exit
+$PYENVBIN update
 #for a in {7..13}; do echo pyenv install --skip-existing $(pyenv latest --known 3.$a) ; done
 a=5
 while [ "${a}" -le 14 ]; do
 	# shellcheck disable=SC2312
-	pyenv install --skip-existing "$(pyenv latest --known 3."${a}")"
+	$PYENVBIN install --skip-existing "$($PYENVBIN latest --known 3."${a}")"
 	a=$((a + 1))
 done
+popd >/dev/null || exit
+rm -rf "$thewrkdir"
