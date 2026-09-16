c68e7dcde95eafad0fd8a0a27451c9adac29b9f8
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2026-09-16 08:27:29 +0200

    optimized

--- LinuxSetup/pipxes.sh
+++ LinuxSetup/pipxes.sh
@@ -1,71 +1,69 @@
 #!/bin/sh
-pipx install archivebox
-pipx install argcomplete
-pipx install asv
-pipx install autopep8
-pipx install bandit
-pipx install beautysh
-pipx install black
-pipx install darker
-pipx install flake8
-pipx install flowmark
-pipx install flynt
-pipx install isort
-pipx install mbake
-####################################################
-pipx install mdformat
-pipx inject mdformat mdformat-beautysh
-pipx inject mdformat mdformat-deflist
-pipx inject mdformat mdformat-dollarmath
-pipx inject mdformat mdformat-footnote
-pipx inject mdformat mdformat-frontmatter
-pipx inject mdformat mdformat-gfm
-pipx inject mdformat mdformat-gfm-alerts
-pipx inject mdformat mdformat-ruff
-pipx inject mdformat mdformat-tables
-pipx inject mdformat mdformat-toc
-####################################################
-pipx install monkeytype
-####################################################
-pipx install mypy
-pipx inject mypy types-psutil
-####################################################
-pipx install openai-whisper
-pipx install pandoc-kroki-filter
-pipx install pandoc-mermaid-filter
-pipx install poetry
-pipx install pre-commit
-pipx install pycodestyle
-pipx install pydocstyle
-pipx install pyflakes
-pipx install pylama
-pipx install pylint
-pipx install pylyzer
-pipx install pyperf
-pipx install pyre
-pipx install pyrefly
-####################################################
-pipx install pytest
-pipx inject pytest pytest-asyncio
-pipx inject pytest pytest-bandit
-pipx inject pytest pytest-beartype
-pipx inject pytest pytest-colordots
-pipx inject pytest pytest-cov
-pipx inject pytest pytest-monkeytype
-pipx inject pytest pytest-mypy
-pipx inject pytest pytest-pyright
-pipx inject pytest pytest-sugar
-pipx inject pytest typeguard
-####################################################
-pipx install pyright
-pipx install pytype
-pipx install pyupgrade
-pipx install rembg
-pipx install ruff
-pipx install sourcery
-pipx install ty
-pipx install ufmt
-pipx install vermin
-pipx install vulture
-##############################################################################################
-# pipx list --include-injected
+#shfmt --write pipxes.sh
+main() {
+	if [ -z "$1" ]; then
+		echo "No function specified. Available functions: manualinstall, export, import"
+	# else run the given function only
+	else
+		$1
+	fi
+}
+main "$@"
+
+manualinstall() {
+	pipx install archivebox
+	pipx install argcomplete
+	pipx install asv
+	pipx install autopep8
+	pipx install bandit
+	pipx install beautysh
+	pipx install black
+	pipx install darker
+	pipx install flake8
+	pipx install flowmark
+	pipx install flynt
+	pipx install isort
+	pipx install mbake
+	pipx install mdformat; pipx inject mdformat mdformat-beautysh mdformat-deflist mdformat-dollarmath mdformat-footnote mdformat-frontmatter mdformat-gfm mdformat-gfm-alerts mdformat-ruff mdformat-tables mdformat-toc
+	pipx install monkeytype
+	pipx install mypy; pipx inject mypy types-psutil
+	pipx install openai-whisper
+	pipx install pandoc-kroki-filter
+	pipx install pandoc-mermaid-filter
+	pipx install poetry
+	pipx install pre-commit
+	pipx install pycodestyle
+	pipx install pydocstyle
+	pipx install pyflakes
+	pipx install pylama
+	pipx install pylint
+	pipx install pylyzer
+	pipx install pyperf
+	pipx install pyre
+	pipx install pyrefly
+	pipx install pyright
+	pipx install pytest; pipx inject pytest pytest-asyncio pytest-bandit pytest-beartype pytest-colordots pytest-cov pytest-monkeytype pytest-mypy pytest-pyright pytest-sugar typeguard
+	pipx install pytype
+	pipx install pyupgrade
+	pipx install rembg
+	pipx install ruff
+	pipx install sourcery
+	pipx install ty
+	pipx install ufmt
+	pipx install vermin
+	pipx install vulture
+}
+
+export() {
+	pipx list --json >pipx_list.json
+	echo "Exported pipx list to pipx_list.json"
+}
+
+import() {
+	if [ -f pipx_list.json ]; then
+		echo "Importing pipx list from pipx_list.json"
+		pipx install-all pipx_list.json
+	else
+		echo "pipx_list.json not found. Please run export first."
+	fi
+}
